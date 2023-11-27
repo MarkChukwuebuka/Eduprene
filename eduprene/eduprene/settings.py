3c5485c123bf4dbb17eb3f42535db5511dda0ac3
Nodebe <nodywelete3@gmail.com>
2023-11-27 19:42:25 +0100

    FIX: Structured well

--- eduprene/eduprene/settings.py
+++ eduprene/eduprene/settings.py
@@ -121,7 +121,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+
+STATIC_URL = os.getenv('STATIC_URL', 'static/')
+STATIC_ROOT = os.getenv('STATIC_ROOT', 'home/app/static/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
