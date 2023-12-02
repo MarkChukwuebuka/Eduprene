6aec76f8ffd7c9a032ab9abdc25ed8eb1059aeb5
Nodebe Ikem <nodywelete3@gmail.com>
2023-12-02 14:29:06 +0100

    Revert "Landing page email collector"

--- eduprene/auth_service/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('register/', views.register, name='register'),
-    path('resend_register_otp/', views.resend_register_otp, name='resend_register_otp')
-]
--- eduprene/landing_page_email_collector/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('email_collector/', views.email, name='email_collector')
-]
